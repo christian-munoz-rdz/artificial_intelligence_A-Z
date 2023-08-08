1159a57077fa5798569dfcbee7b1ba4adfde0b6f
Christian Muñoz <chgeovany.15@gmail.com>
2023-08-07 23:10:06 -0600

    Body finished

--- Module 2 -  Doom/ai.py
+++ Module 2 -  Doom/ai.py
@@ -19,6 +19,8 @@ from ppaquette_gym_doom.wrappers.action_space import ToDiscrete
 import experience_replay, image_preprocessing
 
 # Part 1 - Building the AI
+
+# Making the brain
 class CNN(nn.Module):
 
     def __init__(self, number_actions):
@@ -28,12 +30,41 @@ class CNN(nn.Module):
         self.convolution2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3)
         self.convolution3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 2)
         # Full connection
-        self.fc1 = nn.Linear(in_features = number_neurons, out_features = 40)
+        self.fc1 = nn.Linear(in_features = self.count_neurons((1,80,80)), out_features = 40)
         self.fc2 = nn.Linear(in_features = 40, out_features = number_actions)
+    
+    def count_neurons(self,image_dim):
+        x = Variable(torch.rand(1, *image_dim))
+        x = F.relu(F.max_pool2d(self.convolution1(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution2(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution3(x), 3, 2))
 
-# Making the brain
+        return x.data.view(1, -1).size(1)
+
+    def forward(self, x):
+        # Convolutional layers
+        x = F.relu(F.max_pool2d(self.convolution1(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution2(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution3(x), 3, 2))
+        # Flattening
+        x = x.view(x.size(0), -1)
+        # Full connection
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
 
 # Making the body
+class SoftmaxBody(nn.Module):
+
+    def __init__(self, T):
+        super(SoftmaxBody, self).__init__()
+        self.T = T
+    
+    def forward(self, outputs):
+        probs = F.softmax(outputs * self.T)
+        actions = probs.multinomial()
+        return actions
 
 # Making the AI
 
